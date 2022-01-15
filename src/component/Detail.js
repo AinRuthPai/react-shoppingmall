a427d3fabf6022a8bd86cee4b997389783cc9a83
AinRuthPai <tanbing@naver.com>
2022-01-15 18:57:18 +0900

    scss, useEffect

--- src/component/Detail.js
+++ src/component/Detail.js
@@ -1,9 +1,25 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useHistory, useParams } from "react-router-dom";
+import styled from "styled-components";
+import "../Detail.scss";
 
 function Detail(props) {
   const history = useHistory();
-  let { id } = useParams();
+  const { id } = useParams();
+  const findItem = props.shoes.find(function (item) {
+    return item.id == id;
+  });
+
+  const [alert, changeAlert] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      changeAlert(false);
+    }, 2000);
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [alert]);
 
   return (
     <div className='container'>
@@ -12,9 +28,9 @@ function Detail(props) {
           <img src='https://codingapple1.github.io/shop/shoes1.jpg' width='100%' />
         </div>
         <div className='col-md-6 mt-4'>
-          <h4 className='pt-5'>{props.shoes[id].title}</h4>
-          <p>{props.shoes[id].content}</p>
-          <p>{props.shoes[id].price}</p>
+          <h4 className='pt-5'>{findItem.title}</h4>
+          <p>{findItem.content}</p>
+          <p>{findItem.price}</p>
           <button className='btn btn-danger'>주문하기</button>
           <button
             className='btn btn-danger'
@@ -27,6 +43,11 @@ function Detail(props) {
           </button>
         </div>
       </div>
+      {alert === true ? (
+        <div className='my-alert'>
+          <p>재고가 얼마 남지 않았습니다</p>
+        </div>
+      ) : null}
     </div>
   );
 }
